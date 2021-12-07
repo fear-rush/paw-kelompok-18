cb2762bfc7730b22e7eb85593331fe98f2186e65
Muhammad Firas <muhfiras1@gmail.com>
2021-12-08 00:09:48 +0700

    deleted TodosApp and changed to TodoApp v2

--- Todos App/client/src/components/backdrops.jsx
+++
@@ -1,6 +0,0 @@
-function Backdrop(props){
-    return <div className='fixed z-10 bg-black bg-opacity-50 w-full h-screen top-0 left-0' onClick={props.onCancel}/>
-    //<div class='fixed z-10 bg-black bg-opacity-75 h-screen top-0 left-0'/>;
-}
-
-export default Backdrop;
